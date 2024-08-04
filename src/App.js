9a1beffb11d7cbdb9f9444cd418d10e70cbc818f
Viktor Hraboviuk <zatibmag25565@gmail.com>
2024-08-04 14:05:13 +0200

    Add CardsInfo component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Heading } from "./Components/Heading";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Heading className="big-heading" text="Big Heading" />
+      <Heading className="small-heading" text="Small Heading" />
+      <Heading className="default-heading" text="Default Heading" />
+    </>
+  );
 }
 
 export default App;
