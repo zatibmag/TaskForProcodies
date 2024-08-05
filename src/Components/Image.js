3cda04df6ef4995ab379d9386c9cf38154aa03c1
Viktor Hraboviuk <zatibmag25565@gmail.com>
2024-08-05 10:20:08 +0200

    Update Image.js to have alt

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Css/Image.css";
 
-export function Image({ src, className }) {
-  return <img src={src} className={className} />;
+export function Image({ src, className, alt }) {
+  return <img src={src} className={className} alt={alt} />;
 }
