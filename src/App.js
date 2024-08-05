765f5928db857986559f936467630fd1f9be2b81
Viktor Hraboviuk <zatibmag25565@gmail.com>
2024-08-05 15:57:59 +0200

    Add MiddlePart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { CardsInfo } from "./Components/CardsInfo";
+import { BottomPart } from "./Components/BottomPart";
+import { MiddlePart } from "./Components/MiddlePart";
+
 function App() {
   return (
     <>
+      <MiddlePart />
+      <BottomPart />
       <CardsInfo />
     </>
   );
