59829c0d70c72f954b177b9b8390f8a6290eb065
Viktor Hraboviuk <zatibmag25565@gmail.com>
2024-08-05 10:20:42 +0200

    Update CardsInfo to have alt

--- src/Components/CardsInfo.js
+++ src/Components/CardsInfo.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Text } from "./Text";
 import { Image } from "./Image";
-import cards from "/home/zatibmag/Desktop/for_procodies/src/Images/cards.png";
+import cards from "../Images/cards.png";
 
 export function CardsInfo() {
   return (
     <>
-      <Text
-        className="small-text"
-        text="We accept the following credit cards"
-      />
-      <Image className="small-image" src={cards} />
+      <Text className="card-text" text="We accept the following credit cards" />
+      <Image className="small-image" src={cards} alt="cards" />
     </>
   );
 }
